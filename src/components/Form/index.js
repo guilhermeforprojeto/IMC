fc678ab69b0a198394befe93d4a394bc1aa69ad2
guilhermeforprojeto <guylhermesantos@hotmail.com>
2021-11-04 20:58:06 -0300

    Adicionado componente de resultado do IMC

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import { View, Text, TextInput} from 'react-native';
+import { View, Text, TextInput, Button} from 'react-native';
 
 
 export default function Form(){
     return(
         <View>
+            <View>
 
             <Text>Peso </Text>
             <TextInput
-             placeholder="Digite aqui" 
+             placeholder="Ex.: 1.78" 
              keyboardType="numeric" 
              />
 
-
-
             <Text>Altura</Text>
             <TextInput
-             placeholder="Digite aqui" 
+             placeholder="Ex.: 82" 
              keyboardType="numeric" />
+             <Button title="Calcular IMC "/>
+            </View> 
+            {/* <ResultImc messageResultImc={messageImc} ResultImc={imc}></ResultImc>           */}
+           </View>
 
-        </View>
     )
 }
